478604755328dadb951b70df7e4bec3149248c8a
Kaden Seaman <kadencs24@outlook.com>
2024-12-28 20:46:36 -0700

    basic lexer complete

--- Lexer.ts
+++ Lexer.ts
@@ -1,3 +1,5 @@
+import * as fs from 'fs';
+
 export enum TokenType {
     Number,
     Identifier,
@@ -8,6 +10,10 @@ export enum TokenType {
     Let,
 }
 
+const KEYWORDS: Record<string, TokenType> = {
+    'let': TokenType.Let,
+}
+
 export interface Token {
     value: string;
     type: TokenType;
@@ -15,6 +21,20 @@ export interface Token {
 
 const token = (value = '', type: TokenType): Token => {
     return { value, type };
+}
+
+const isInteger = (src: string) => {
+    const c = src.charCodeAt(0);
+    const BOUNDS = ['0'.charCodeAt(0), '9'.charCodeAt(0)];
+    return (c >= BOUNDS[0] && c <= BOUNDS[1]);
+}
+
+const isalpha = (src: string) => {
+    return src.toUpperCase() !== src.toLowerCase();
+}
+
+const isSkippable = (src: string) => {
+    return src === ' ' || src === '\n' || src === '\t';
 }
 
 export const tokenize = (sourceCode: string): Token[] => {
@@ -30,11 +50,48 @@ export const tokenize = (sourceCode: string): Token[] => {
         } else if (src[0] === '+' || src[0] === '-' || src[0] === '/' || src[0] === '*'){
             tokens.push(token(src.shift(), TokenType.BinaryOperator));           
         } else if (src[0] === '='){
-            tokens.push(token(src.shift(), TokenType.BinaryOperator));           
+            tokens.push(token(src.shift(), TokenType.Equals));           
+        } else {
+            // Handle Mutlicharacter tokens
+            
+            // Build Number Token
+            if(isInteger(src[0])){
+                let num = '';
+                while(src.length > 0 && isInteger(src[0])){
+                    num += src.shift();
+                }
+
+                tokens.push(token(num, TokenType.Number));
+            }
+            else if(isalpha(src[0])){
+                let ident = '';
+                while(src.length > 0 && isalpha(src[0])){
+                    ident += src.shift();
+                }
+
+                //check for reserved keywords
+                const reserved = KEYWORDS[ident];
+                if(reserved === undefined){
+                    tokens.push(token(ident, TokenType.Identifier));
+                }
+                else {
+                    tokens.push(token(ident, reserved));
+                }
+            }
+            else if(isSkippable(src[0])){
+                src.shift();
+            }
+            else{
+                console.log('Unrecognized character found in srouce: ', src[0]);
+                process.exit(1);
+            }
         }
     }
 
     return tokens;
 }
 
-
+const sourceCode = fs.readFileSync('testing.txt', 'utf-8');
+for(const token of tokenize(sourceCode)){
+    console.log(token);
+};
